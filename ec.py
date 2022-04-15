44e1ac7b9e1bfe9cb078dc8c380359d1ad318413
Ertugrul Dalboy <ertugrul.dalboy@ing.com>
2022-04-16 00:05:51 +0200

    Bug fixed

--- ec.py
+++ ec.py
@@ -34,9 +34,8 @@ if __name__ == '__main__':
     #image_directory = input("Please enter the directory that may contain the image: ")
 
     image_name = "kus.jpeg"
-    image_directory = "/Users/lg48np/Projects/dp-examples/easy_cartonify/pictures/"
+    image_directory = "pictures/"
 
-    os.chdir(Path(image_directory))
     cartoon_style_selection = input("This script currently has 2 sytles. Please type 1 or 2. ")
     
     cartonify = Cartonify(image_directory, image_name)
